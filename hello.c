4641c505b6c4a1a36b354aa2b514557d815ad0af
kpham14 <kpham15@huskers.unl.edu>
2021-08-26 18:30:45 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ken Pham and Leo Hernandez
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ken Pham and Leo Hernandez!\n");
+  printf("Computer Engineering")
+
+  return 0;
+}
